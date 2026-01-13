724094f74841cb0fb3a04d21583c98265a872726
hirooaoy <haoyama@adobe.com>
2026-01-13 13:32:46 -0800

    Initial Commit

---
+++ Goodnight Journal/Goodnight_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goodnight_JournalApp.swift
+//  Goodnight Journal
+//
+//  Created by Hiroo Aoyama on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Goodnight_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Goodnight JournalTests/Goodnight_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goodnight_JournalTests.swift
+//  Goodnight JournalTests
+//
+//  Created by Hiroo Aoyama on 1/13/26.
+//
+
+import Testing
+@testable import Goodnight_Journal
+
+struct Goodnight_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
